eb47fd2984201ef296d9b69d44dfdeaeed90592d
corpsepk <corpsepk@gmail.com>
2018-03-13 11:52:33 +0300

    fixed test (dep yii 2.0.14)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,1 @@
-<?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
-
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-Yii::setAlias('@tests', __DIR__);
-
-new \yii\console\Application([
-    'id' => 'unit',
-    'basePath' => __DIR__,
-    'vendorPath' => __DIR__ . '/../vendor',
-    'components' => [
-        'assetManager' => [
-            'class' => 'tests\AssetManager',
-            'basePath' => '@tests/assets',
-            'baseUrl' => '/',
-        ],
-    ],
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
-]);
+<?phpdefined('YII_DEBUG') or define('YII_DEBUG', true);defined('YII_ENV') or define('YII_ENV', 'test');require(__DIR__ . '/../vendor/autoload.php');require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');Yii::setAlias('@tests', __DIR__);new \yii\console\Application([    'id' => 'unit',    'basePath' => __DIR__,    'vendorPath' => __DIR__ . '/../vendor',    'components' => [        'assetManager' => [            'class' => 'tests\AssetManager',            'basePath' => '@tests/assets',            'baseUrl' => '/',        ],    ],    'aliases' => [        '@bower' => '@vendor/bower-asset',        '@npm'   => '@vendor/npm-asset',    ],]);
